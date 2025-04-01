261b0f86be0bdee97d221aa4067bb7cfbd405865
Janhavi03 <158798982+Janhavi123098@users.noreply.github.com>
2025-04-01 18:07:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Analytics from "./Analytics"; // Importing Analytics Page
+
+function App() {
+  return (
+    <div className="App">
+      <Analytics />  {/* Rendering the Analytics Page */}
+    </div>
+  );
+}
+
+export default App;
